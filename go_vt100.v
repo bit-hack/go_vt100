062b8c196ecb4b5428a479e5c2445842b306f2de
aidan <aidan@codeplay.com>
2020-01-15 17:28:55 +0000

    Sketchout basic memory mapping

--- go_vt100.v
+++ go_vt100.v
@@ -2,20 +2,56 @@
 `timescale 1ns / 1ps
 
 
+// 8x1280 rom
+//    (~3 SB_RAM40_4K)
+module rom_8x1280(input i_clk,
+                  input [10:0] i_addr,
+                  output reg [7:0] o_data);
+
+  reg [7:0] rom_reg [1280];
+  initial $readmemh("../vt100_chr.txt", rom_reg);
+
+  always @(posedge i_clk) begin
+    o_data <= rom_reg[i_addr];
+  end
+endmodule
+
+
 // VT100 character rom
-module vt100_rom(input [6:0] i_char,
+module vt100_rom(input i_clk,
+                 input [6:0] i_char,
                  input [2:0] i_x,
                  input [3:0] i_y,
                  output reg o_bit);
 
-  reg [7:0] rom_chr [1280];
-  initial $readmemh("../vt100_chr.txt", rom_chr);
-
   wire [10:0] rom_addr = i_y * 128 + i_char; 
-  wire [7:0] rom_cell = rom_chr[ rom_addr ];
+  wire [7:0] rom_cell;
+  rom_8x1280 rom(i_clk, rom_addr, rom_cell);
 
   always @* begin
     o_bit = rom_cell[ ~i_x ];
+  end
+endmodule
+
+
+// VT100 screen buffer
+// 8x80x24 = 8x1920
+//    (~4 SB_RAM40_4K)
+module vt100_sbuffer(input i_clk,
+                     input [10:0] i_wr_addr,
+                     input [7:0]  i_wr_data,
+                     input i_wr,
+                     input [10:0] i_rd_addr,
+                     output reg [7:0] o_rd_data);
+
+  reg [7:0] scr_ram [ 1920 ];
+  initial $readmemh("../vt100_scr.txt", scr_ram);
+
+  always @(posedge i_clk) begin
+    if (i_wr) begin
+      scr_ram[i_wr_addr] <= i_wr_data;
+    end
+    o_rd_data <= scr_ram[i_rd_addr];
   end
 endmodule
 
@@ -27,6 +63,8 @@ module vga_t(input i_clk,
              output reg o_vsync,
              output reg [9:0] o_x,
              output reg [9:0] o_y,
+             output reg [4:0] row_y,
+             output reg [4:0] chr_y,
              output reg o_blank);
 
 //  Clock Summary 
@@ -45,6 +83,16 @@ module vga_t(input i_clk,
     end else begin
       o_x <= o_x + 10'd1;
     end
+  end
+
+  always @(posedge i_clk) begin
+    row_y <= (o_y   == 10'd479) ? 0 :
+             (row_y ==  5'd19)  ? 0 : (row_y + 5'd1);
+  end
+
+  always @(posedge i_clk) begin
+    chr_y <= (o_y   == 10'd479) ? 0 :
+             (row_y ==  5'd19)  ? (chr_y + 4'd1) : chr_y;
   end
 
   always @(posedge i_clk) begin
@@ -83,21 +131,28 @@ module go_vga(input        i_Clk,
 
   wire [9:0] vga_x;
   wire [9:0] vga_y;
+  wire [4:0] vga_y_row;               // 2x oversampled
+  wire [6:0] vga_x_chr = vga_x[10:4]; // 2x oversampled
+  wire [4:0] vga_y_chr;
   wire vga_blank;
+  vga_t vga(i_Clk, o_VGA_HSync, o_VGA_VSync, vga_x, vga_y, vga_y_row, vga_y_chr, vga_blank);
 
-  vga_t vga(i_Clk, o_VGA_HSync, o_VGA_VSync, vga_x, vga_y, vga_blank);
+  reg [10:0] ram_wr_addr;
+  reg [7:0] ram_wr_data;
+  reg ram_wr;
+  wire [10:0] ram_rd_addr = (vga_y_chr * 80) + vga_x_chr;
+  wire [7:0] ram_char;
+  vt100_sbuffer scr_ram(i_Clk, ram_wr_addr, ram_wr_data, ram_wr, ram_rd_addr, ram_char);
 
   wire [2:0] rom_x = vga_x[2:0];
-  wire [3:0] rom_y = vga_y[3:0];
+  wire [3:0] rom_y = vga_y_row[4:1];  // upscale 2x
   wire rom_bit;
-  vt100_rom chr_rom(character, rom_x, rom_y, rom_bit);
+  // note: we add 3'd1 to the rom_x here so that the rom will look-ahead
+  // giving us the correct bits when we need it.
+  vt100_rom chr_rom(i_Clk, ram_char[6:0], rom_x + 3'd1, rom_y, rom_bit);
 
-  assign o_VGA_R = vga_blank ? 3'd0 : (rom_bit ? 3'b111 : 3'b000);
-  assign o_VGA_G = vga_blank ? 3'd0 : (rom_bit ? 3'b111 : 3'b000);
-  assign o_VGA_B = vga_blank ? 3'd0 : (rom_bit ? 3'b111 : 3'b000);
-
-  reg [6:0] character = 0;
-  always @(posedge i_Switch[0]) begin
-    character <= character + 7'd1;
-  end
+  wire [3:0] rgb = vga_blank ? 3'd0 : (rom_bit ? 3'b111 : 3'b000);
+  assign o_VGA_R = rgb;
+  assign o_VGA_G = rgb;
+  assign o_VGA_B = rgb;
 endmodule
